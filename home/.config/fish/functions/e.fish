d2351012755ed26b650669c8cff21209a9eef051
NNB <nnbnh@protonmail.com>
2020-11-08 12:25:38 +0700

    modified:   home/.config/fish/functions/e.fish 	deleted:    home/.config/godot/editor_settings-3.tres 	deleted:    home/.config/gtk-2.0/gtkfilechooser.ini 	deleted:    home/.config/gtk-2.0/gtkrc 	deleted:    home/.config/gtk-3.0/bookmarks 	deleted:    home/.config/gtk-3.0/colors.css 	deleted:    home/.config/gtk-3.0/gtk.css 	deleted:    home/.config/gtk-3.0/settings.ini 	modified:   home/.config/imv/config 	new file:   home/.config/kak/colors/bui.kak 	modified:   home/.config/kak/kakrc 	deleted:    home/.local/bin/baniwa 	modified:   packageslist 	modified:   setup

--- home/.config/fish/functions/e.fish
+++ home/.config/fish/functions/e.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
-function v --wraps='$EDITOR' --wraps=kak --description 'alias v $EDITOR'
+function e --wraps="$EDITOR" --description "alias v $EDITOR"
   $EDITOR $argv;
 end
