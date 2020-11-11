7d848691e4861deb6a1ec2c6f54c08a03f70f755
NNB <nnbnh@protonmail.com>
2020-11-11 18:05:36 +0700

    deleted:    home/.config/fish/functions/e.fish 	deleted:    home/.config/fish/functions/l.fish 	new file:   home/.config/fish/functions/ll.fish

--- home/.config/fish/functions/e.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 1
-function e --wraps="$EDITOR" --description "alias v $EDITOR"
-  $EDITOR $argv;
-end
--- home/.config/fish/functions/l.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 1
-function l --wraps=ls --wraps='exa --all --group-directories-first --long --header --git' --description 'alias l exa --all --group-directories-first --long --header --git'
-  exa --all --group-directories-first --long --header --git $argv;
-end
