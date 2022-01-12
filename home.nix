5da62445baaa914c32dbc547753ce4b9db5ba7ab
NNB <nnbnh@protonmail.com>
2022-01-12 20:12:57 +0700

    modified:   home.nix

--- home.nix
+++ home.nix
@@ -24,47 +24,50 @@ in {
     ".local/share/fonts/bmono".source = "${bmono}/dist/bmono/ttf";
     ".config/wayfire.conf".text = ''
       [core]
-      plugins = command scale move resize alpha grid autostart
+      plugins = command scale move resize alpha wrot grid autostart
       background_color = 0.13671875 0.15625 0.2421875 1.0
-      
+
       close_top_view = <super> KEY_BACKSPACE
-      
-      
+
       [command]
-      
       binding_terminal = <super> KEY_ENTER
       command_terminal = kitty
-      
+
       binding_switch_input = <super> KEY_SPACE
       command_switch_input = fcitx-remote -t
-      
+
       repeatable_binding_volume_up = KEY_VOLUMEUP
       command_volume_up = amixer set Master 5%+
       repeatable_binding_volume_down = KEY_VOLUMEDOWN
       command_volume_down = amixer set Master 5%-
       binding_mute = KEY_MUTE
       command_mute = amixer set Master toggle
-      
+
       repeatable_binding_light_up = KEY_BRIGHTNESSUP
       command_light_up = brightnessctl set 5%+
       repeatable_binding_light_down = KEY_BRIGHTNESSDOWN
       command_light_down = brightnessctl set 5%-
-      
-      
+
+      binding_screen_shot = <super> KEY_S
+      command_screen_shot = grim
+
       [scale]
       duration = 180
       inactive_alpha = 0.5
       toggle = <super>
-      
+
       [move]
       activate = <super> BTN_LEFT
-      
+
       [resize]
       activate = <super> BTN_RIGHT
-      
+
       [alpha]
       modifier = <super>
-      
+
+      [wrot]
+      activate = <super> BTN_MIDDLE
+
       [autostart]
       autostart_wf_shell = false
       language_input = fcitx
