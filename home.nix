5aa8f448eb2372d020b617d1ca14b2057f1869da
NNB <nnbnh@protonmail.com>
2022-02-06 11:17:22 +0000

    modified:   home.nix 	modified:   tty.nix

--- home.nix
+++ home.nix
@@ -4,12 +4,12 @@
   imports = [ ./tty.nix ];
 
   home.packages = with pkgs; [
-    berry sarasa-gothic xdotool
     ungoogled-chromium blender godot
-    (pkgs.writeScriptBin "wm" "exec startx $(which berry)")
+    (pkgs.writeScriptBin "wm" "startx $(which berry)") berry
     (pkgs.writeScriptBin "bye" "systemctl suspend")
     (pkgs.writeScriptBin "lock" "${pkgs.i3lock}/bin/i3lock --color 000000")
-    (pkgs.writeScriptBin "menu4all" "kitty") #TODO
+    (pkgs.writeScriptBin "menu4all" "rofi -show drun") #TODO
+    sarasa-gothic # CJK support for font
   ];
 
   home.file = {
@@ -17,22 +17,11 @@
 
     ".config/berry/autostart".text = ''
       #!/bin/sh
-      berryc border_width 2
-      berryc inner_border_width 0
-      berryc title_height 24
-      berryc set_font Bmono-12
+      berryc decorate_new false
       berryc edge_gap 0 0 0 0
-      berryc focus_color 5890F8
-      berryc unfocus_color 525866
-      berryc inner_focus_color 5890F8
-      berryc inner_unfocus_color 525866
-      berryc text_focus_color 171726
-      berryc text_unfocus_color 171726
-      berryc resize_mask "mod4"
+      #TODO berryc resize_mask "mod4|ctrl"
       picom &
       ${pkgs.xwallpaper}/bin/xwallpaper --zoom ${builtins.fetchurl "https://raw.githubusercontent.com/NNBnh/wallpapers/main/i5yal/colorful.png"} &
-      xdotool behave_screen_edge top-left exec menu4all &
-      xdotool behave_screen_edge top-right exec berryc fullscreen &
       ${pkgs.xcape}/bin/xcape -e "Super_L=Super_L|s"
       sxhkd &
       fcitx &
@@ -52,44 +41,40 @@
 
   services = {
     picom.enable = true;
-
     sxhkd = {
       enable = true;
       keybindings = {
         "XF86Audio{Mute,RaiseVolume,LowerVolume}" = "amixer set Master {toggle,5%+,5%-}";
         "XF86MonBrightness{Up,Down}" = "${pkgs.brightnessctl}/bin/brightnessctl set 5%{+,-}";
         "{_,ctrl} + {_,shift} + Print" = "{_,region | }{shot,record}"; #TODO
-        "~button{1,2,3,4,5}" = "berryc pointer_focus";
+        "~button{1,2,3}" = "berryc pointer_focus";
         "super + button{4,5}" = "picom-trans -c -- {+,-}5";
-        "super + {Tab,Return,Up,Down,Left,Right}" = "berryc {cycle_focus,fullscreen,window_monocle,window_close,snap_left,snap_right}";
+        "super + {Tab,Up,Down,Left,Right}" = "berryc {cycle_focus,window_monocle,window_close,snap_left,snap_right}";
         "super + s" = "menu4all";
       };
     };
-
     screen-locker = {
       enable = true;
       lockCmd = "lock";
     };
   };
 
-  i18n.inputMethod = { #FIXME
+  i18n.inputMethod = {
     enabled = "fcitx";
     fcitx.engines = with pkgs.fcitx-engines; [ unikey ];
   };
 
   programs = {
-    rofi = { #TODO
+    rofi = {
       enable = true;
       font = "Bmono 12";
     };
-
     kitty = {
       enable = true;
       font = { name = "Bmono"; size = 10; };
       settings = {
         disable_ligatures = "cursor";
         clear_all_shortcuts = true;
-        allow_remote_control = true;
       };
       keybindings = {
         "super+c" = "copy_to_clipboard";
--- tty.nix
+++ tty.nix
@@ -24,26 +24,20 @@
     bash = {
       enable = true;
       historyFile = ".cache/bash_history";
-      bash_history = ["erasedups", "ignorespace"];
+      historyControl = ["erasedups" "ignorespace"];
       shellAliases = {
         l = "ls --almost-all --group-directories-first";
         dl = "trash-put";
         md = "mkdir --parents";
         ex = "patool extract";
         ar = "patool create";
-        e = "hx";
+        e = "$EDITOR";
         g = "git";
       };
       bashrcExtra = ''
         for env in EDITOR VISUAL PAGER MANPAGER; do eval export $env=hx; done
         theme
-        export PS1="\e]0;\w\a\e[0;90m \w\e[0m\n\033[1;94m❯ "
-        d() { cd $@; l }
-
-        # @events.on_postcommand
-        # def print_exitcode(cmd, rtn, out, ts, **kw):
-        #   if rtn:
-        #     print(f"\033[7;91m E:{str(rtn)} \033[0m")
+        export PS1="\[\e]0;\w\a\e[0;90m\] \w\[\e[0m\]\n\[\033[1;94m\]❯\[\e[m\] " #TODO exitcode display
       '';
     };
     git = {
