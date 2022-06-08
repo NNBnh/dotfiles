8adb086b9c84efcea990c6d74fefc17c07f7966c
NNB <nnbnh@protonmail.com>
2022-06-08 15:20:03 +0000

    Update run_once_setup.sh

--- run_once_setup.sh
+++ run_once_setup.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
 # TODO bmono
-sudo paru --sync --skipreview --useask --noconfirm --needed \
+paru --sync --skipreview --useask --noconfirm --needed \
 	nextcloud-client ibus-unikey ttf-sarasa-gothic \
 	kitty librewolf-bin blender godot \
 	steam retroarch multimc-bin osu-lazer-bin \
-	ruby xonsh neovim \
-	btop trash-cli p7zip ffmpeg git
+	ruby xonsh neovim trash-cli p7zip ffmpeg git
 
 chsh -s $(which xonsh)
