1bacca9dc3be80b1302d2a1e9ab83e2cecbf7e71
NNB <nnbnh@protonmail.com>
2022-06-22 15:05:45 +0000

    Add Gnome and Paru installer

--- run_once_setup.sh
+++ run_once_setup.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
+sudo pacman --sync --sysupgrade --refresh --needed git base-devel
+git clone https://aur.archlinux.org/paru-bin.git ~/.cache/paru
+cd ~/.cache/paru
+makepkg --syncdeps --install --noconfirm
+
 paru --sync --skipreview --useask --noconfirm --needed \
 	chezmoi nextcloud-client \
-	ibus-unikey ttf-bmono ttf-sarasa-gothic \
-	kitty librewolf-bin blender godot \
+	gnome ibus-unikey ttf-bmono ttf-sarasa-gothic \
+	kitty firefox blender godot \
 	steam multimc-bin osu-lazer-bin \
 	retroarch retroarch-assets-ozone \
 		libretro-mesen libretro-bsnes libretro-mupen64plus-next libretro-dolphin \
@@ -11,4 +16,13 @@ paru --sync --skipreview --useask --noconfirm --needed \
 		libretro-sameboy libretro-mgba libretro-desmume libretro-citra \
 	ruby xonsh neovim trash-cli p7zip ffmpeg git
 
+for user_directories in DOCUMENTS PICTURES VIDEOS MUSIC DESKTOP DOWNLOAD PUBLICSHARE TEMPLATES; do
+	xdg-user-dirs-update --set $user_directories ~
+done
+
 chsh -s $(which xonsh)
+
+sudo systemctl enable gdm.service
+sudo systemctl enable bluetooth.service
+
+systemctl reboot
