4bbafd3813e5a512e769b173f99640b57625f7df
NNB <nnbnh@protonmail.com>
2022-06-22 14:31:19 +0000

    Add Retroarch packages

--- run_once_setup.sh
+++ run_once_setup.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
 paru --sync --skipreview --useask --noconfirm --needed \
-	nextcloud-client ibus-unikey ttf-bmono ttf-sarasa-gothic \
+	chezmoi nextcloud-client \
+	ibus-unikey ttf-bmono ttf-sarasa-gothic \
 	kitty librewolf-bin blender godot \
-	steam retroarch multimc-bin osu-lazer-bin \
+	steam multimc-bin osu-lazer-bin \
+	retroarch retroarch-assets-ozone \
+		libretro-mesen libretro-bsnes libretro-mupen64plus-next libretro-dolphin \
+		libretro-duckstation libretro-pcsx2 \
+		libretro-sameboy libretro-mgba libretro-desmume libretro-citra \
 	ruby xonsh neovim trash-cli p7zip ffmpeg git
 
 chsh -s $(which xonsh)
