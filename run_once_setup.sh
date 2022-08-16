37a4285cb58141112f9c3570102974a569f4281a
NNB <nnbnh@protonmail.com>
2022-08-17 00:03:27 +0700

    Update run_once_setup.sh

--- run_once_setup.sh
+++ run_once_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo dnf install --assumeyes flatpak ruby xonsh neovim btop trash-cli p7zip git ffmpeg
+sudo dnf install --assumeyes papirus-icon-theme flatpak ruby xonsh neovim btop trash-cli p7zip git ffmpeg
 
 flatpak install flathub org.wezfurlong.wezterm org.mozilla.firefox com.usebottles.bottles org.blender.Blender org.godotengine.Godot com.valvesoftware.Steam org.libretro.RetroArch org.polymc.PolyMC sh.ppy.osu
 
