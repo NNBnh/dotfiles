c57f5faa3e77b1317856f8445d1f95892b406253
NNB <nnbnh@protonmail.com>
2022-08-21 01:37:31 +0700

    Update run_once_setup.sh

--- run_once_setup.sh
+++ run_once_setup.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
-sudo dnf install --assumeyes papirus-icon-theme flatpak ruby xonsh neovim btop trash-cli p7zip git ffmpeg
+sudo dnf install --assumeyes papirus-icon-theme ibus-unikey ruby xonsh neovim trash-cli p7zip ffmpeg
 
-flatpak install flathub org.wezfurlong.wezterm org.mozilla.firefox com.usebottles.bottles org.blender.Blender org.godotengine.Godot com.valvesoftware.Steam org.libretro.RetroArch org.polymc.PolyMC sh.ppy.osu
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak install flathub \
+	org.wezfurlong.wezterm org.mozilla.firefox com.usebottles.bottles \
+	org.blender.Blender org.godotengine.Godot \
+	com.valvesoftware.Steam org.libretro.RetroArch org.polymc.PolyMC sh.ppy.osu
 
 chsh -s $(which xonsh)
+
+git config --global user.name nnbnh@protonmail.com
+git config --global user.name NNB
+git config --global credential.helper store
