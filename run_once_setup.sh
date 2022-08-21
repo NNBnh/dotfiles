8221f1afc66ae09df134830a8fd4c254879c5dd1
NNB <nnbnh@protonmail.com>
2022-08-22 01:21:26 +0700

    Update run_once_setup.sh

--- run_once_setup.sh
+++ run_once_setup.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
 sudo dnf copr enable --assumeyes peterwu/iosevka
-sudo dnf install --assumeyes \
-	papirus-icon-theme iosevka-curly-fonts ibus-unikey \
-	ruby xonsh neovim trash-cli p7zip ffmpeg-free
+sudo dnf install --assumeyes papirus-icon-theme iosevka-curly-fonts ibus-unikey
+
+gsettings set org.gnome.desktop.interface icon-theme 'Papirus Dark'
 
 sudo dnf remove --assumeyes gnome-terminal firefox
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install --assumeyes flathub \
-	org.wezfurlong.wezterm org.mozilla.firefox com.usebottles.bottles \
+	org.wezfurlong.wezterm com.usebottles.bottles \
 	org.blender.Blender org.godotengine.Godot \
 	com.valvesoftware.Steam org.libretro.RetroArch org.polymc.PolyMC sh.ppy.osu
 
-chsh -s $(which xonsh)
+brew install ruby nushell neovim p7zip ffmpeg imagemagick gifski
 
 git config --global user.email nnbnh@protonmail.com
 git config --global user.name NNB
