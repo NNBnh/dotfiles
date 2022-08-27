e188da8b2180b1c0e4b0020e0f378db0589ee145
NNB <nnbnh@protonmail.com>
2022-08-27 10:22:50 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -14,7 +14,28 @@ flatpak install --assumeyes flathub \
 	com.visualstudio.code org.blender.Blender org.godotengine.Godot \
 	com.valvesoftware.Steam org.libretro.RetroArch org.polymc.PolyMC sh.ppy.osu
 
-# TODO wezterm
+mkdir -p ~/.var/app/org.wezfurlong.wezterm/config/wezterm
+cat > ~/.var/app/org.wezfurlong.wezterm/config/wezterm/wezterm.lua << CONFIG
+local wezterm = require 'wezterm'
+
+return {
+  font = wezterm.font 'Iosevka Curly Extended',
+  font_size = 10.0,
+  color_scheme = 'Catppuccin Mocha',
+  bold_brightens_ansi_colors = false,
+  use_fancy_tab_bar = false,
+  hide_tab_bar_if_only_one_tab = true,
+  keys = {
+    { key =    'UpArrow', mods = 'CTRL|SHIFT', action = 'DisableDefaultAssignment' },
+    { key =  'DownArrow', mods = 'CTRL|SHIFT', action = 'DisableDefaultAssignment' },
+    { key =  'LeftArrow', mods = 'CTRL|SHIFT', action = 'DisableDefaultAssignment' },
+    { key = 'RightArrow', mods = 'CTRL|SHIFT', action = 'DisableDefaultAssignment' },
+    { key =          'Z', mods = 'CTRL|SHIFT', action = wezterm.action.SendKey { key = 'Y',     mods = 'CTRL' } },
+    { key =      'Enter', mods =      'SHIFT', action = wezterm.action.SendKey { key = 'Enter', mods = 'ALT'  } },
+  },
+  default_prog = {'/home/linuxbrew/.linuxbrew/bin/nu'},
+}
+CONFIG
 
 
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
